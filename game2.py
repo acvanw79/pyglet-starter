a17dfebe53b794b7c8300ddc9a39563798fe4661
Adam VanWyk <60404099+acvanw79@users.noreply.github.com>
2020-02-14 14:31:58 -0500

    moving and staying inside screen

--- game2.py
+++ game2.py
@@ -2,18 +2,27 @@ import pyglet
 
 win= pyglet.window.Window()
 
+hp = 100
+
 # Create a sprite so the game can keep track of coordinates
 img= pyglet.image.load('assets/hero/sliced/idle-3.png')
 spr = pyglet.sprite.Sprite(img, x = 200, y = 200)
 
 spritesheet= pyglet.image.load('assets/hero/Old hero.png')
-smol_img = pygame.Surface([16, 16])
-smol_img.blit(spritesheet, (0, 0), (16, 0, 16, 16))
+
+# create some texe
+tittle = pyglet.text.Label('Mr. Fantastic', x = 275, y = 465)
+text = pyglet.text.Label(text = "100", x = 0, y = 465)
+
+
 
 # Get the key state handler object
 keys = pyglet.window.key.KeyStateHandler()
 
 def update(dt):
+    global hp
+    text.text = str(hp)
+    print(hp)
     win.push_handlers(keys)             # update the keys object
     if keys[pyglet.window.key.LEFT]: # if the key is pressed...
         spr.x -= 1                                # change spr.x by -1
@@ -23,11 +32,23 @@ def update(dt):
         spr.y += 1
     if keys[pyglet.window.key.DOWN]:
         spr.y -= 1
+    if spr.x < 0:
+        hp -= 1
+    if spr.x > 630:
+        hp -= 1
+    if spr.y < 0:
+        hp -= 1
+    if spr.y > 470:
+        hp -= 1
+
 
 @win.event
 def on_draw():
     win.clear()
-    spr.draw()
+    if hp > 0:
+        spr.draw()
+    text.draw()
+    
 
 pyglet.clock.schedule(update) 
 pyglet.app.run()
